ec39ba052f4408e78f97bc68fc7222d827ade6e3
ana clara valerio carneiro <ana.carneiro-youxlab@institutoyoux.org.br>
2026-04-15 17:21:10 -0300

    06-SomaDeNumeros

---
+++ logicaProgramacao/06-TiposPrimitivosSaidaDados/06- SomaDeNumeros.py
@@ -0,0 +1,9 @@
+n1 = int(input('digite um numero'))
+n2 = int(input('digite mais um numero'))
+s = n1 + n2
+print('a soma vale',s)
+
+#int= 7; -4; 0; 9875
+#float= 4,5 ; 0.076; -15.223; 7.0
+#bool= true; False
+#str= 'Olá' ;'7.5' ;''
