f9f53e3efbcd04372e745a5f8d6aa5fcd6a82cd7
allana santos <allana.silva-youxlab@institutoyoux.org.brgit config --global user.email allana.silva-youxlab@institutoyoux.org.br>
2026-04-15 17:54:28 -0300

    estudo py

---
+++ logica_python/04_primeiros_comandos/ex002.py
@@ -0,0 +1,2 @@
+nome = input (' digite seu nome:')
+print ('é um prazer te conhecer, {} !'. format (nome))
---
+++ logica_python/05_instalando_pycharm/desafio002.py
@@ -0,0 +1,5 @@
+#DESAFIO002:
+#fazendo um programa que leia o nome da pessoa e mostre uma mensagem de boas-vindas.
+
+nome = input ('digite seu nome : ')
+print('é um peazer te conhecer {}!'. format(nome))
