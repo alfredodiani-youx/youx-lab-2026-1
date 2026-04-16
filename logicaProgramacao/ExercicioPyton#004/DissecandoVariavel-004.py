96e20c9205cc895b47dca337be4dbf3c8fc6b6c3
ana clara valerio carneiro <ana.carneiro-youxlab@institutoyoux.org.br>
2026-04-16 15:03:37 -0300

    DissecandoVariavel-004.py

---
+++ logicaProgramacao/ExercicioPyton#004/DissecandoVariavel-004.py
@@ -0,0 +1,10 @@
+#Faça um programador que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informaçoes possiveis sobre ele.
+a = input('Digite algo: ')
+print('O tipo primitivo desse valor é ', type(a))
+print('Só tem espaços? ',a.isspace())
+print('É um numero? ' ,a.isnumeric())
+print('É alfabetico? ', a.isalpha())
+print('É alfanumerico? ', a.isalnum())
+print('Esta em minusculas? ',a.isupper())
+print('Esta em minusculas? ',a.islower())
+print('Esta capitalizada? ', a.istitle())
