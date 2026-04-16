89204bd1875b2ff02c680f7d03bb7565b72964d5
ana clara valerio carneiro <ana.carneiro-youxlab@institutoyoux.org.br>
2026-04-16 17:03:26 -0300

    OrdemDePrecedencia007.py

---
+++ logicaProgramacao/07-OperadoresAritmeticos/OrdemDePrecedencia007.py
@@ -0,0 +1,18 @@
+#1 ()
+#2 **
+#3 * / // (divisao)
+#4 + -
+
+n1 = int(input('Um valor: '))
+n2 = int(input('Outro valor: '))
+print('A soma vale {}'.format(n1+n2))
+
+n1 = int(input('Um valor: '))
+n2 = int(input('Outro valor: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print('A soma é {}, o produto é {} e a divisao é {}'.format(s, m, d))
+print('Divisão inteira {} e potencia {}'.format(di, e))
