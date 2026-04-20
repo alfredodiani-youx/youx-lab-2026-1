e31a1fdeab0d2f64b027b3644a695d7fb9435711
ana clara valerio carneiro <ana.carneiro-youxlab@institutoyoux.org.br>
2026-04-20 13:32:06 -0300

    desafio005.py

--- logicaProgramacao/07-OperadoresAritmeticos/desafio005.py
+++ logicaProgramacao/07-OperadoresAritmeticos/desafio005.py
@@ -1,1 +1,6 @@
 #Faça um programa que leia um numero inteiro e mostre na tela o seu sucessor e se antecesspr.
+
+n = int(input('Digite um numero: '))
+a = n - 1
+s = n + 1
+print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n, a, s))
