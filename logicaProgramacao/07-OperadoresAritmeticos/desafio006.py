48898a257bea5d7e467b13e216b5a3834a22af7e
ana clara valerio carneiro <ana.carneiro-youxlab@institutoyoux.org.br>
2026-04-20 13:53:23 -0300

    desafio006.py

--- logicaProgramacao/07-OperadoresAritmeticos/desafio006.py
+++ logicaProgramacao/07-OperadoresAritmeticos/desafio006.py
@@ -1,1 +1,8 @@
 #Crie um algoritmo que leia um numero e mostre o seu dobro,triplo e raiz quadrada.
+
+n = int(input('Digite um numero: '))
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+print('O dobro de {} vale {}.'.format(n, d))
+print('O triplo de {} vale {}. /n A raiz quadrada de {} é igual a {:.2f}.'.format(n, t, n, r))
