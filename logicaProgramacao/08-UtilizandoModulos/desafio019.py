9cf6ccd17c1826795730a6baa8aa9225b4f522bc
ana clara valerio carneiro <ana.carneiro-youxlab@institutoyoux.org.br>
2026-04-21 17:15:42 -0300

    desafio019

--- logicaProgramacao/08-UtilizandoModulos/desafio019.py
+++ logicaProgramacao/08-UtilizandoModulos/desafio019.py
@@ -1,2 +1,13 @@
 #Um professor quer sortear um dos seus quatro alunos para apagar o quadro.
 #Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
+
+#DESAFIO019
+
+from math import radians, sin, cos, tan
+angulo = float(input('Digite o angulo que voce deseja: '))
+seno = sin(radians(angulo))
+print('O angulo de {} tem o SENO de {:.2f}'.format(angulo, seno))
+cosseno = cos(radians(angulo))
+print('O angulo de {} tem o COSSENO de {:.2f}'.format(angulo, cosseno))
+tangente = tan(radians(angulo))
+print('O angulo de {} tem a TANGETE de {:.2f}'.format(angulo, tangente))
