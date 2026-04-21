b24136eb179ee9109d8875cbd05ce6c7d891c556
ana clara valerio carneiro <ana.carneiro-youxlab@institutoyoux.org.br>
2026-04-21 17:06:55 -0300

    desafio017.py

--- logicaProgramacao/08-UtilizandoModulos/desafio017.py
+++ logicaProgramacao/08-UtilizandoModulos/desafio017.py
@@ -1,2 +1,9 @@
 #Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triangulo retangulo,
 #calcule e mostre o comprimento da hipotenusa.
+#DESAFIO017
+
+from math import hypot
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adiacente: '))
+hi = hypot(co, ca)
+print('A hipotenusa vai medir {:.2f}'.format(hi))
