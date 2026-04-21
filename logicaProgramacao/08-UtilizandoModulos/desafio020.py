acc09391a4e36816bf8350b605aae7e435df2938
ana clara valerio carneiro <ana.carneiro-youxlab@institutoyoux.org.br>
2026-04-21 19:08:23 -0300

    desafio029.py

--- logicaProgramacao/08-UtilizandoModulos/desafio020.py
+++ logicaProgramacao/08-UtilizandoModulos/desafio020.py
@@ -1,2 +1,12 @@
 #O mesmo professor do desafio anterior quer sortear a ordem de apresentaçao de trabalhos dos alunos.
 #Faça um programa que leia o nome dos quatros alunos e mostre a ordem sorteada.
+
+import random
+n1 = str(input('Primeiro aluno: '))
+n2 = str(input('Segundo aluno: '))
+n3 = str(input('Terceiro aluna: '))
+n4 = str(input('Quarto aluno: '))
+lista = [n1, n2, n3, n4]
+random.shuffle(lista)
+print('A ordem de apresentaçao sera ')
+print(lista)
