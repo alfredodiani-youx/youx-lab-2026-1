65d70a1bccadd75deb262b7c67efb76e48f6e634
ana clara valerio carneiro <ana.carneiro-youxlab@institutoyoux.org.br>
2026-04-22 17:14:21 -0300

    desafio028.py

---
+++ logicaProgramacao/010-Condiçoes(parte1)/desafio028.py
@@ -0,0 +1,14 @@
+#Escreva um programa que faça o computador "pensar" em um numero inteiro entre 0 e 5 e peça para o usuario tentar descobrir.
+# qual foi o numero escolhido pelo computador.
+#O programa deverá escrever na tela se o usuario venceu ou perdeu.
+
+from random import randint
+computador = randint(0, 5) # Faz o computador "PENSAR"
+print('-=-' * 20)
+print('Vou pensar em um numero entre 0 e 5.Tente adivinhar...')
+print('-=-' * 20)
+jogador = int(input('Em que numero eu pensei? ')) # Jogador tenta advinhar
+if jogador == computador:
+    print('PARABENS!Você conseguiu me vencer!')
+else:
+    print('GANHEI! Eu pensei no numero {} e nao no {}!'.format(computador, jogador))
