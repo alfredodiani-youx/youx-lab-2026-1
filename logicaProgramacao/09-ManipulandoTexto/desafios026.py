4846fdeda87d1a8c7f8fbe16f11962595dc51429
ana clara valerio carneiro <ana.carneiro-youxlab@institutoyoux.org.br>
2026-04-21 21:52:56 -0300

    desafio026.py

--- logicaProgramacao/09-ManipulandoTexto/desafios026.py
+++ logicaProgramacao/09-ManipulandoTexto/desafios026.py
@@ -2,3 +2,8 @@
 #-Quantas vezes aparece a letra "A"
 #-Em que posiçao ela aparece a primeira vez.
 #-Em que posiçao ela aparece a ultima vez.
+
+frase = str(input('Digite uma frase: ')).upper().strip()
+print('A letra A aparece {} vezes na frase.'.format(frase.count('A')))
+print('A primeira letra A apareceu na posiçao {}'.format(frase.find('A')+1))
+print('A ultima letra A apareceu na posiçao {}'.format(frase.rfind('A')+1))
