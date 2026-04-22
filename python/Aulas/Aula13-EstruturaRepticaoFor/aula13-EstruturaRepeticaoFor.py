1379b7109e3ddfe6bf4fed054f05ecff74df32b6
ArturYouX <arthur.souza-youxlab@institutoyoux.org.br>
2026-04-22 13:47:18 -0300

    Adicionando parte prática da aula 13 e pastas para os exercícios

---
+++ python/Aulas/Aula13-EstruturaRepticaoFor/aula13-EstruturaRepeticaoFor.py
@@ -0,0 +1,27 @@
+for c in range(1,6):
+    print('Oi!')
+print('cabou')
+
+for c in range(1, 6):
+    print(c)
+print('cabou')
+
+for c in range(6, 0, -1):
+    print(c)
+print('cabou')
+
+for c in range(0,10, 2):
+    print(c)
+print('cabou')
+
+n = int(input('Digite um número: '))
+for c in range(0, n+1):
+    print(c)
+print('FIM')
+
+i = int(input('Início: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+for c in range(i, f+1, p):
+    print(c)
+print('FIM')
