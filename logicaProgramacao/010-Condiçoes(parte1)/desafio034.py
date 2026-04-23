b88ae7b1c5a5328398135aa260643998d37ec06c
ana clara valerio carneiro <ana.carneiro-youxlab@institutoyoux.org.br>
2026-04-23 15:52:38 -0300

    desafio034.py

--- logicaProgramacao/010-Condiçoes(parte1)/desafio034.py
+++ logicaProgramacao/010-Condiçoes(parte1)/desafio034.py
@@ -1,3 +1,10 @@
 #Escreva um programa que pergunte o salario de um funcionario e calcula o valor do seu aumento.
 #Para salarios superiores a R$1.250,00, calcule um aumento de 10%.
 #Para os inferiores ou iguais, o aumento é de 15%.
+
+salario = float(input('Qual é o salario do funcionario? R$'))
+if salario <= 1250:
+    novo = salario + (salario * 15 / 100)
+else:
+    novo = salario + (salario * 10 / 100)
+print('Quem ganhava R${:.2f} passa a ganhar R${:.2f} agora.'.format(salario, novo))
