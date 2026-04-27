51ddbc446c57f7fe7e349f1eda5e0fda8045a78b
ana clara valerio carneiro <ana.carneiro-youxlab@institutoyoux.org.br>
2026-04-27 17:55:51 -0300

    desafio042.py

--- Mundo2/Desafio042.py
+++ Mundo2/Desafio042.py
@@ -2,3 +2,17 @@
 #- Equilatero:Todos os lados iguais.
 #- Isoceles:Dois lados iguais.
 #-Escaleno:Todos os lados diferentes.
+
+r1 = float(input('Primeiro segmento: '))
+r2 = float(input('Segundo segmneto: '))
+r3 = float(input('Terceiro segmento: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('O segmento acima PODEM FORMAR um triangulo ', end='')
+    if r1 == r2 == r3:
+        print('EQUILATERO!')
+    elif r1 != r2 != r3:
+        print('ESCALENO!')
+    else:
+        print('ISOCELES')
+else:
+    print('Os segmentos acima NAO PODEM FORMAR triangulo')
