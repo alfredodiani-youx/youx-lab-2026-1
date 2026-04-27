35e1a0c2d3aae4a71e3900c88f7b9d18c998b8a2
ana clara valerio carneiro <ana.carneiro-youxlab@institutoyoux.org.br>
2026-04-27 17:09:51 -0300

    Desafio041.py

--- Mundo2/Desafio041.py
+++ Mundo2/Desafio041.py
@@ -1,6 +1,23 @@
+#Classificando Atletas.
 #A Confederaçao Nacional de Nataçao precisa de um programa que leia o ano de nascimento de um atleta e mostre sua categoria, de acordo com a idade:
 #-Ate 9 anos:MIRIM
 #Ate 14 anos:INFANTIL
 #Ate 19 anos:JUNIOR
 #Ate 20 anos:SENIOR
 #Acima:MASTER
+
+from datetime import date
+atual = date.today().year
+nascimento = int(input('Ano de Nascimento: '))
+idade = atual - nascimento
+print('O atleta tem {} anos.'.format(idade))
+if idade <= 9:
+    print('Classificaçao: MIRIM')
+elif idade <= 14:
+    print('Classificaçao: INFANTIL')
+elif idade <= 19:
+    print('Classificaçao: JUNIOR')
+elif idade <=25:
+    print('Classisficaçao: SENIOR')
+else:
+    print('Classificaçao: MASTER')
