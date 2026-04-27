9b807f04800a7d18fc81e532dc892f62ac95350b
Ana Clara <ana.diniz-youxlab@institutoyoux.org.br>
2026-04-27 15:52:57 -0300

    aula 9

---
+++ logicaProgramacao/aula-09/ex022.py
@@ -0,0 +1,6 @@
+nome = str(input('Digite seu nome:')).strip()
+print ('analisando seu nome. . .')
+print ('Seu nome em maiusculas e {}'.format(nome.upper()))
+print ('Seu nome em minusculas e {}'.format(nome.lower()))
+print ('Seu nome ao todo tem {} letras'.format(len(nome) -nome.count(' ')))
+print ('Seu primeiro nome tem {} letras'.format(nome.find(' ')))
---
+++ logicaProgramacao/aula-09/ex027.py
@@ -0,0 +1,6 @@
+n = str(input('Digite seu nome completo: ')).strip()
+nome = n.split()
+print(nome)
+print ('Muito prazer em te conhecer!')
+print ('Seu primeiro nome e {}'.format(nome[0]))
+print ('Seu ultimo nome e {}'.format(nome[-1]))
