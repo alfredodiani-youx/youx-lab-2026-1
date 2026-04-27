96db7bd6dfeeee5ef79e633283e24b68748841ef
ana clara valerio carneiro <ana.carneiro-youxlab@institutoyoux.org.br>
2026-04-27 16:59:23 -0300

    Desafio038.py

--- Mundo2/Desafio038.py
+++ Mundo2/Desafio038.py
@@ -2,3 +2,12 @@
 # -O primeiro valor é maior.
 # -O segundo valor é maior.
 # -Nao existe valor maior, os dois sao iguais.
+
+n1 = int(input('Primeiro numero: '))
+n2 = int(input('Segundo numero: '))
+if n1 > n2:
+    print('O PRIMEIRO valor é maior')
+elif n2 > n1:
+    print('O SEGUNDO valor é maior')
+else:
+    print('Os dois valores sao IGUAIS')
--- Mundo2/Desafio040.py
+++ Mundo2/Desafio040.py
@@ -2,3 +2,10 @@
 # -Media abaixo de 5.0: REPROVADO.
 # -Media entre 5.0 e 6.9: RECUPERAÇAO.
 # -Media 7.0 ou superior: APROVADO.
+
+nota1 = float(input('Primeira nota: '))
+nota2 = float(input('Segunda nota: '))
+media = (nota1 + nota2) / 2
+print('Tirando {:.1f} e {:.1f}, a media do aluno é {:.1f}'.format(nota1, nota2, media))
+if media >=5 and media < 7:
+    print('O aluno esta em RECUPERAÇAO.')
