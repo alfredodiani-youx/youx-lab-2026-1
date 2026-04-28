4e1dbcf8fe5806384e317fa1d43a9e8597df757c
ana clara valerio carneiro <ana.carneiro-youxlab@institutoyoux.org.br>
2026-04-28 16:09:17 -0300

    desafio045

--- Mundo2/Desafio045.py
+++ Mundo2/Desafio045.py
@@ -1,1 +1,23 @@
-#Crie um programa que faça o computador jogar Jokenpô com vocẽ.
+# Crie um programa que faça o computador jogar Jokenpô com vocẽ.
+import random
+
+print('''suas opcoes
+[0] pedra
+[1] papel
+[2] tesoura''')
+
+escolhaJogador = int(input('Qual é a sua jogada?'))
+escolhaComputador = random.randint(0, 2)
+print(escolhaComputador)
+if escolhaJogador == escolhaComputador:
+    print('Empate')
+elif escolhaJogador == 0 and escolhaComputador == 2:
+    print('Voce escolheu pedra e o computador escolheu tesoura, VOCE GANHOU!')
+elif escolhaJogador == 1 and escolhaComputador == 0:
+    print('Voce escolheu papel e o computador escolheu pedra, VOCE GANHOU!')
+elif escolhaJogador == 2 and escolhaComputador == 1:
+    print('Voce escolheu tesoura e o computador escolheu papel, VOCE GANHOU!')
+else:
+    print('O computador Ganhou')
+
+
