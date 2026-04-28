fdeed8e346446a47b0867357884843d2e64791ad
ana clara valerio carneiro <ana.carneiro-youxlab@institutoyoux.org.br>
2026-04-28 13:48:50 -0300

    Desafio043.py

--- Mundo2/Desafio043.py
+++ Mundo2/Desafio043.py
@@ -4,3 +4,18 @@
 #25 ate 30:Sobrepeso.
 #30 ate 40:Obesidade.
 #Acima de 40:Obesidade morbida.
+
+peso = float(input('Qual é o seu peso? (Kg)'))
+altura = float(input('Qual é a sua altura? (m)'))
+imc = peso / (altura ** 2)
+print('O IMC dessa pessoa é {:.1f}'.format(imc))
+if imc < 18.5:
+    print('Você esta ABAIXO do peso normal!')
+elif 18.5 <= imc < 25:
+    print('PARABENS,você está na faixa de PESO NORMAL!')
+elif 25 <= imc < 30:
+    print('Você está em SOBREPESO!')
+elif 30 <= imc < 40:
+    print('Você está em OBESIDADE, cuidado!')
+elif imc >= 40:
+    print('Você está em OBESIDADE MORBIDA, cuidado!')
