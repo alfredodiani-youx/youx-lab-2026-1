b1ba7c2ee3048394ada65214ddc7376e951a44e3
KauanMartins496 <kauan.martins-youxlab@institutoyoux.org.br>
2026-04-29 13:34:48 -0300

    Exercicio 38

---
+++ logicaProgramacao/Ex038.py
@@ -0,0 +1,8 @@
+Numero1 = int(input('Digite seu numero: '))
+Numero2 = int(input('Digite seu segundo numero: '))
+if Numero1 > Numero2:
+    print('O PRIMEIRO valor e maior')
+elif Numero1 < Numero2:
+    print('O SEGUNDO valor e maior')
+else:
+    print(f'Os numeros sao iguais')
