14c4904aab34ba266396d86b1e4b28b67e3572a2
KauanMartins496 <kauan.martins-youxlab@institutoyoux.org.br>
2026-05-11 17:52:47 -0300

    Exercicio 75

---
+++ logicaProgramacao/Ex075.py
@@ -0,0 +1,2 @@
+tupla = (int(input("Digite o primeiro número: ")), int(input("Digite o segundo número: ")), int(input("Digite o terceiro número: ")), int(input("Digite o quarto número: ")))
+print(tupla)
