95c4e5d4e3e0e115a2187de6f74cb03000faee81
ana clara valerio carneiro <ana.carneiro-youxlab@institutoyoux.org.br>
2026-05-21 17:47:41 -0300

    exercicios

--- logicaProgramacao/Mundo_3/exercicio083,py.py
+++ logicaProgramacao/Mundo_3/exercicio083,py.py
@@ -1,8 +1,18 @@
 #Exercício Python 083: Crie um programa onde o usuário digite uma expressão qualquer que use parênteses.
 #Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
 
-expr = str(input('Digite uma expresa'))
+expr = str(input('Digite uma expressao: '))
 pilha = []
 for simb in expr:
     if simb == '(':
         pilha.append('(')
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressao esta valida!')
+else:
+    print('Sua expressao esta errada!')
