8b67384d29c799a2797fad0c818b7d1c2c580744
KauanMartins496 <kauan.martins-youxlab@institutoyoux.org.br>
2026-05-26 16:28:32 -0300

    DesafioPython

---
+++ logicaProgramacao/DesafioPython.py
@@ -0,0 +1,52 @@
+funcionarios = [
+    {"Nome": "José",      "Salario": 1500, "anos_trabalhados": 6},
+    {"Nome": "Maria",     "Salario": 1800, "anos_trabalhados": 4},
+    {"Nome": "João",      "Salario": 2200, "anos_trabalhados": 8},
+    {"Nome": "Ana",       "Salario": 2000, "anos_trabalhados": 3},
+    {"Nome": "Carlos",    "Salario": 2500, "anos_trabalhados": 10},
+    {"Nome": "Paula",     "Salario": 2300, "anos_trabalhados": 5},
+    {"Nome": "Marcos",    "Salario": 1900, "anos_trabalhados": 2},
+    {"Nome": "Fernanda",  "Salario": 2600, "anos_trabalhados": 7},
+    {"Nome": "Rafael",    "Salario": 2100, "anos_trabalhados": 4},
+    {"Nome": "Clara",     "Salario": 2400, "anos_trabalhados": 6},
+    {"Nome": "Bruno",     "Salario": 1700, "anos_trabalhados": 1},
+    {"Nome": "Luiza",     "Salario": 2800, "anos_trabalhados": 9},
+    {"Nome": "Felipe",    "Salario": 1950, "anos_trabalhados": 3},
+    {"Nome": "Camila",    "Salario": 2250, "anos_trabalhados": 5},
+    {"Nome": "Ricardo",   "Salario": 2700, "anos_trabalhados": 11},
+    {"Nome": "Patrícia",  "Salario": 2100, "anos_trabalhados": 4},
+    {"Nome": "Gustavo",   "Salario": 2600, "anos_trabalhados": 8},
+    {"Nome": "Larissa",   "Salario": 1850, "anos_trabalhados": 2},
+    {"Nome": "Tiago",     "Salario": 2350, "anos_trabalhados": 6},
+    {"Nome": "Renata",    "Salario": 2550, "anos_trabalhados": 7},
+    {"Nome": "Diego",     "Salario": 2000, "anos_trabalhados": 3},
+    {"Nome": "Beatriz",   "Salario": 2450, "anos_trabalhados": 5},
+    {"Nome": "André",     "Salario": 1900, "anos_trabalhados": 2},
+    {"Nome": "Aline",     "Salario": 2300, "anos_trabalhados": 4},
+    {"Nome": "Fábio",     "Salario": 2750, "anos_trabalhados": 9},
+    {"Nome": "Sofia",     "Salario": 2150, "anos_trabalhados": 3},
+    {"Nome": "Eduardo",   "Salario": 2650, "anos_trabalhados": 8},
+    {"Nome": "Carolina",  "Salario": 2050, "anos_trabalhados": 4},
+    {"Nome": "Henrique",  "Salario": 2500, "anos_trabalhados": 6},
+]
+
+
+
+#Pra cada funcionario,ele verifica se os anos trabalhados foram mais que 5 e se sim adiciona o aumento
+for funcionario in funcionarios:
+    if funcionario["anos_trabalhados"] > 5:
+        Porcentagem =  funcionario["Salario"] * 20 / 100
+        R =  funcionario["Salario"] + Porcentagem
+    #se for menor do que 5 ele so aumenta 10%
+    else:
+        Porcentagem = funcionario["Salario"] * 10 / 100
+        R =  funcionario["Salario"]+ Porcentagem
+
+
+
+    novo_salario = {"Novo_Salario": R}
+    funcionarios.append(novo_salario)
+    print(funcionarios)
+
+#anotacoes:
+#Nao estou conseguindo acessar as chaves especificas e colocar na area certa a chave novo_salario
